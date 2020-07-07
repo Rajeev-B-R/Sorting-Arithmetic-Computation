f7639b9b3ddc552d04600d978be6cc243b39b818
Rajeev <rajeevsuchiha@gmail.com>
2020-07-07 13:17:43 +0530

    Added: Code to compute (a%b)+c added

--- sortArithCompProb.sh
+++ sortArithCompProb.sh
@@ -9,8 +9,9 @@ read -p "Enter value of c:" c
 val1=`expr $a + $b '*' $c`
 val2=`expr $a '*' $b + $c`
 val3=`expr $c + $a / $b`
-
+val4=`expr $a % $b + $c`
 
 echo "a+(b*c):" $val1
 echo "(a*b)+c:" $val2
 echo "c+(a/b):" $val3
+echo "(a%b)+c:" $val4
