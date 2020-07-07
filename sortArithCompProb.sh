56313315f61ff05637c1ccd8c3e3541bbe43b062
Rajeev <rajeevsuchiha@gmail.com>
2020-07-07 13:12:44 +0530

    Added: Code to compute c+(a/b) added

--- sortArithCompProb.sh
+++ sortArithCompProb.sh
@@ -8,6 +8,9 @@ read -p "Enter value of c:" c
 
 val1=`expr $a + $b '*' $c`
 val2=`expr $a '*' $b + $c`
+val3=`expr $c + $a / $b`
+
 
 echo "a+(b*c):" $val1
 echo "(a*b)+c:" $val2
+echo "c+(a/b):" $val3
